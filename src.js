87553918dfb1b814932d0cf9079d740af1bb4d8d
Harry Hendry <harryhendry89@hotmail.co.uk>
2024-01-17 09:01:47 +0000

    help

---
+++ src.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function() {
+    const Navbar = document.querySelector(".navbar");
+    Navbar.classList.toggle("active");
+}
+
